a2d746ca310879640a81d7c673702d8cbd6efb2c
jdcra <jdcrawford100@gmail.com>
2022-12-29 14:19:17 -0800

    Playlist menu can procedurally create and register submenus

--- src/gui/CMenuBar.java
+++ src/gui/CMenuBar.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -89,14 +90,32 @@ public class CMenuBar extends JMenuBar implements ActionListener {
 		
 		// TODO procedurally fill with created playlists prefixed with "Delete "
 		
-		delete_playlist_submenu.add(new JLabel("  Empty  "));
+		//delete_playlist_submenu.add(new JLabel("  Empty  "));
 		
 		// initialize and fill "View Playlist" submenu
 		view_playlist_submenu = new JMenu("View Playlist");
 		
 		// TODO procedurally fill with created playlists prefixed with "Open "
-
-		view_playlist_submenu.add( new JLabel("  Empty  "));
+		// Start test playlist list
+		ArrayList<String> test_list = new ArrayList<String>();
+		JMenuItem temp_del, temp_view;
+		if (test_list.size() > 0) {
+			for (int i = 0; i < test_list.size(); i++) {
+				temp_del = new JMenuItem(test_list.get(i));
+				temp_del.addActionListener(this);
+				temp_view = new JMenuItem(test_list.get(i));
+				temp_view.addActionListener(this);
+				delete_playlist_submenu.add(temp_del);
+				view_playlist_submenu.add(temp_view);
+			}
+			
+		} else {
+			view_playlist_submenu.add(new JLabel("  Empty  "));
+			delete_playlist_submenu.add(new JLabel("  Empty  "));
+		}
+		// End test playlist list
+		
+		//view_playlist_submenu.add( new JLabel("  Empty  "));
 		
 		// fills playlist menu with entries and submenus
 		playlist_menu.add(new_playlist_menuitem);
@@ -117,32 +136,40 @@ public class CMenuBar extends JMenuBar implements ActionListener {
 	public void actionPerformed(ActionEvent selection) {
 		// TODO Auto-generated method stub
 		
-		Object selection_source = selection.getSource();
-		
-		if (selection_source == new_file_menuitem) {
+		if (selection.getSource() == new_file_menuitem) {
 			System.out.println("New File Clicked");
 		}
-		if (selection_source == new_dir_menuitem) {
+		if (selection.getSource() == new_dir_menuitem) {
 			System.out.println("New Directory Clicked");
 		}
-		if (selection_source == set_default_dir_menuitem) {
+		if (selection.getSource() == set_default_dir_menuitem) {
 			System.out.println("Set Default Directory Clicked");
 			// TODO prompt user to enter new default directory, save to JSON
 		}
-		if (selection_source == exit_menuitem) {
+		if (selection.getSource() == exit_menuitem) {
 			System.out.println("Exit Clicked");
 			Connoisseur.getInstance().getWindow().dispose();
 		}
-		if (selection_source == edit_tags_menuitem) {
+		if (selection.getSource() == edit_tags_menuitem) {
 			System.out.println("Edit Tags Clicked");
 			// TODO prompt user to enter tag(s) to apply to 
 		}
-		if (selection_source == new_playlist_menuitem) {
+		if (selection.getSource() == new_playlist_menuitem) {
 			System.out.println("New Playlist Clicked");
 			// TODO prompt user for tag to create playlist, and save playlist to JSON
 		}
-		// TODO create a loop to check for "Delete <playlist tag>"
-		// TODO create a loop to check for "View <playlist tag>"
+		for(int i = 0; i < delete_playlist_submenu.getItemCount(); i++) {
+			if (selection.getSource() == delete_playlist_submenu.getItem(i)) {
+				System.out.println(selection.getActionCommand() + " Delete Clicked");
+				// TODO prompt user to confirm deletion before deleting playlist from json
+			}
+		}
+		for(int i = 0; i < view_playlist_submenu.getItemCount(); i++) {
+			if (selection.getSource() == view_playlist_submenu.getItem(i)) {
+				System.out.println(selection.getActionCommand() + " View Clicked");
+				// TODO display contents of playlist from json
+			}
+		}
 	}
 
 }
